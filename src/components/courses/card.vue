ccf054e19d01dd5cd648b97e6e1f8e02f95a9a0e
HhamzaN <0956818979h@gmail.com>
2022-07-22 01:25:24 +0300

    courses

---
+++ src/components/courses/card.vue
@@ -0,0 +1,56 @@
+<template>
+  <v-card class="mx-3 pa-2 rounded-lg dark2 mb-10 card" max-width="260">
+    <div style="height: min-content; overflow: hidden">
+      <v-img c :src="image" height="200px" class="rounded-lg image"></v-img>
+    </div>
+
+    <v-card-title class="white--text mb-3"> {{ title }}</v-card-title>
+
+    <v-card-subtitle class="white--text caption color">
+      <p>{{ discription }}</p>
+    </v-card-subtitle>
+
+    <v-card-actions class="justify-end">
+      <a class="white--text text-decoration-underline download mr-1">
+        Download
+      </a>
+
+      <a class="dark2">
+        <v-icon color="blue" small> mdi-download</v-icon>
+      </a>
+    </v-card-actions>
+  </v-card>
+</template>
+<script>
+export default {
+  props: {
+    image: String,
+    title: String,
+    discription: String,
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+<style lang="scss">
+.card {
+  cursor: pointer;
+}
+.image {
+  transition: all 0.3s ease;
+}
+.card:hover {
+  .image {
+    transform: scale(1.1);
+  }
+}
+.download {
+  font-size: 12px;
+}
+.color {
+  p {
+    color: rgba(216, 216, 216, 0.975);
+  }
+}
+</style>
