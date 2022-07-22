f55bad07d7fb9610e6bcc3391c8dc66aa5f04db4
HBO30 <diab.4869@gmail.com>
2022-07-22 21:15:47 +0300

    test

--- src/App.vue
+++ src/App.vue
@@ -86,7 +86,7 @@ export default {
 <style lang="scss">
 @import url("https://fonts.googleapis.com/css2?family=Cairo:wght@300;500;700&display=swap");
 @import "./style/global.scss";
-
+// ll
 * {
   font-family: "Cairo", sans-serif;
 }
